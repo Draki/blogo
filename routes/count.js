f51b6ea7e8b043bcd0e00dc4bfc06e1eba2de89e
Daniel Rodriguez <daniel.rodriguez.rodriguez@alumnos.upm.es>
2013-06-07 13:06:35 +0200

    cont 1a1

--- routes/count.js
+++ routes/count.js
@@ -2,7 +2,9 @@
 var cont = 0;
 exports.count_mw = function(){
         return function (req,res,next){
-                cont++;
+                if ((req.path != '/images/bg.jpg')&(req.path != '/stylesheets/style.css')) {
+			cont++;
+		}
                 console.log("Visitas: " + cont);
                 next();
         }
