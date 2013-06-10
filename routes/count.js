5b58e69319d3f697141f3ee2f1a0c3cec95f35a2
Daniel Rodriguez <daniel.rodriguez.rodriguez@alumnos.upm.es>
2013-06-10 08:26:28 +0200

    fav7

--- routes/count.js
+++ routes/count.js
@@ -1,18 +1,19 @@
-
 var cont = 0;
-exports.count_mw = function(){
-        return function (req,res,next){
-                if ((req.path != '/images/bg.jpg')
-                	&&(req.path != '/stylesheets/style.css')
-                	&&(req.path != '/misc/movie.mp4')
-                	&&(req.path != '/stylesheets/bootstrap.min.css')
-                	&&(req.path != '/js/bootstrap.min.js')) {
-			cont++;
+exports.count_mw = function() {
+	return function(req, res, next) {
+		if ((req.path != '/images/bg.jpg') 
+			&& (req.path != '/img/unfavourite.jpg') 
+			&& (req.path != '/img/favourite.jpg') 
+			&& (req.path != '/stylesheets/style.css') 
+			&& (req.path != '/misc/movie.mp4') 
+			&& (req.path != '/stylesheets/bootstrap.min.css') 
+			&& (req.path != '/js/bootstrap.min.js')) {
+				cont++;
 		}
-                console.log("Visitas: " + cont);
-                next();
-        }
+		console.log("Visitas: " + cont);
+		next();
+	}
 }
-exports.getCount = function (){
-        return cont;
+exports.getCount = function() {
+	return cont;
 }
